1f9759eba56123c618e2b080ce15dd8957120012
Abu Naser Dipu <abunaserdipu0@gmail.com>
2021-03-29 11:26:47 +0600

    burj-al-arab-website-launch

---
+++ src/components/Login/Login.js
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
+import { UserContext } from "../../App";
+import { useHistory, useLocation } from "react-router";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const Login = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+
+  const { from } = location.state || { from: { pathname: "/" } };
+
+  const handleGoogleSignIn = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        var { displayName, email } = result.user;
+        const signedInUser = { name: displayName, email };
+        setLoggedInUser(signedInUser);
+        storeAuthToken();
+        history.replace(from);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+  };
+
+  const storeAuthToken = () => {
+    firebase
+      .auth()
+      .currentUser.getIdToken(/* forceRefresh */ true)
+      .then(function (idToken) {
+        sessionStorage.setItem("token", idToken);
+      })
+      .catch(function (error) {
+        // Handle error
+      });
+  };
+  return (
+    <div>
+      <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTvK7nKGUOqRdEujbkPYH5maH6R6jrbCc",
+    authDomain: "burj-al-arabwithreact.firebaseapp.com",
+    projectId: "burj-al-arabwithreact",
+    storageBucket: "burj-al-arabwithreact.appspot.com",
+    messagingSenderId: "1039352144188",
+    appId: "1:1039352144188:web:caa13fd78a774005b1fe39"
+  };
+
+  export default firebaseConfig;
